349feee174eeef8a57715996befca3c29dbf42bc
BurhancCc <burhan.canbaz@Windesheim.nl>
2025-11-27 16:55:12 +0100

    Created project solution in Visual Studio 2022

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace csharp_groep31.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
